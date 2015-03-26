e57e82e5b4e501f4cfd05a8c9d27071eecebac76
hollybertolet <hbertolet@gmail.com>
2015-03-26 17:52:00 -0400

    Updating

--- js/menu.js
+++ js/menu.js
@@ -2,17 +2,23 @@ angular.module('menu', ['xml'])
   .config(function ($httpProvider) {
     $httpProvider.interceptors.push('xmlHttpInterceptor');
   })
+ 
   .controller('MenuCtrl', function ($scope, $http) {
-    $http.get('muhlenberg_dining.xml').success(function (data) {
+    $http.get('https://bergdining.herokuapp.com/menu.json?location=Wood%20Dining%20Commons').success(function (data) {
         console.log(data);
-    	if (Array.isArray(data.dining.locations.location)) {
+        $scope.menu = data;
+        /*$scope.menu = JSON.parse(data); 
+      /* https://bergdining.herokuapp.com/menu.json?location=Wood%20Dining%20Commons */
+        
+    	/*if (Array.isArray(data.dining.locations.location)) {
     		$scope.locations = data.dining.locations.location;
     	} else {
     		$scope.locations = data.dining.locations;
     	}
       
-
+    */
     });
   });
 
-var accvalue = 0;
+
+
